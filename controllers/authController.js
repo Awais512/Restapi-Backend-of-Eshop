f0ded6b6b1837e1b6ea8219030653bb8f7636163
awais512 <raza.awais25@gmail.com>
2021-03-08 14:58:54 +0500

    Hashing the password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const { User } = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 //@desc     Create User
 //@route    POST /api/v1/users
@@ -8,7 +9,7 @@ exports.register = async (req, res) => {
     let user = new User({
       name: req.body.name,
       email: req.body.email,
-      passwordHash: req.body.passwordHash,
+      passwordHash: bcrypt.hashSync(req.body.password, 10),
       phone: req.body.phone,
       isAdmin: req.body.isAdmin,
       street: req.body.street,
