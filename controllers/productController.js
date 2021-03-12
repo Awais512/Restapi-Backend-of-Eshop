b1270beff1387f8ef3af2ae2135f0456f1b78964
awais512 <raza.awais25@gmail.com>
2021-03-12 13:10:42 +0500

    Validatingt file type

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,17 +3,27 @@ const { Category } = require('../models/category');
 const mongoose = require('mongoose');
 const multer = require('multer');
 
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+};
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads');
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error('Invalid File Type');
+    if (isValid) {
+      uploadError = null;
+    }
+    cb(uploadError, 'public/uploads');
   },
   filename: function (req, file, cb) {
     const fileName = file.originalname.split(' ').join('-');
+    const extension = FILE_TYPE_MAP[file.mimetype];
     cb(null, `${fileName}_${Date.now()}.${extension}`);
   },
 });
-
-const upload = multer({ storage: storage });
 
 //@desc     Create Product
 //@route    POST /api/v1/products
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const multer = require('multer');
 
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg',
+};
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/uploads');
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error('Invalid File Type');
+    if (isValid) {
+      uploadError = null;
+    }
+    cb(uploadError, 'public/uploads');
   },
   filename: function (req, file, cb) {
     const fileName = file.originalname.split(' ').join('-');
+    const extension = FILE_TYPE_MAP[file.mimetype];
     cb(null, `${fileName}_${Date.now()}.${extension}`);
   },
 });
