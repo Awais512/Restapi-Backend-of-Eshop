b4fd13bad60469e89cbdab0a500a3742b24ea0ba
awais512 <raza.awais25@gmail.com>
2021-03-12 13:14:41 +0500

    Image upload with request

--- controllers/productController.js
+++ controllers/productController.js
@@ -32,6 +32,8 @@ exports.createProduct = async (req, res) => {
   try {
     let category = await Category.findById(req.body.category);
     if (!category) return res.status(400).send('Invalid Category');
+    const file = req.file;
+    if (!file) return res.status(400).send('No image in the request');
     const fileName = req.file.filename;
     const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
     let product = new Product({
